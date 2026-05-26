f706f0d5f99ac549fa1c6876472fcdf71ed78e9a
Alex Lein <alein@trakitwireless.com>
2026-05-26 16:03:43 -0400

    Updated way of contructing the user and the "self" account details

--- Commands/Accounts/Self/Responses/RepSelfGet.cs
+++ Commands/Accounts/Self/Responses/RepSelfGet.cs
@@ -15,26 +15,19 @@ namespace Trakit.Commands {
 		/// </summary>
 		public DateTime? expiry;
 		/// <summary>
-		/// This session's <see cref="User"/> details (if the service is being used by a <see cref="User"/>).
-		/// If this value is not present, then the session is not yet authenticated.
+		/// The UTC date/time of the server hosting the connection.
 		/// </summary>
-		public SelfUser user;
+		public readonly DateTime serverTime;
+
 		/// <summary>
 		/// This <see cref="Machine"/>'s details (if the service is being used by a <see cref="Machine"/>).
 		/// If this value is not present, then the session is not a machine account.
 		/// </summary>
 		public SelfMachine machine;
 		/// <summary>
-		/// This <see cref="User"/>'s <see cref="CompanyPolicies.sessionPolicy"/>.
+		/// This session's <see cref="User"/> details (if the service is being used by a <see cref="User"/>).
+		/// If this value is not present, then the session is not yet authenticated.
 		/// </summary>
-		public SessionPolicy sessionPolicy;
-		/// <summary>
-		/// This <see cref="User"/>'s <see cref="CompanyPolicies.passwordPolicy"/>.
-		/// </summary>
-		public PasswordPolicy passwordPolicy;
-		/// <summary>
-		/// The UTC date/time of the server hosting the connection.
-		/// </summary>
-		public DateTime serverTime;
+		public SelfUser user;
 	}
 }
