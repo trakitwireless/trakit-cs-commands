82fc74dbd87b5f20f643775f3d412ca79169ed03
Alex Lein <alein@trakitwireless.com>
2026-05-27 09:21:29 -0400

    We're skipping the whole "different asset classes" thing now

--- Commands/API/JSON/ConvertAsset.cs
+++ Commands/API/JSON/ConvertAsset.cs
@@ -11,70 +11,23 @@ namespace Trakit.Tools {
 	public class ConvertAsset : TrakitConverter<Asset> {
 		public override Asset ConvertFrom(JsonReader reader, Type type, Asset asset, bool existing, JsonSerializer serializer) {
 			var obj = JObject.Load(reader);
-			if (!Enum.TryParse(obj["kind"]?.ToString(), true, out AssetType kind)) throw new JsonException();
 			if (
 				bool.TryParse(obj["deleted"]?.ToString(), out _)
 				|| bool.TryParse(obj["suspended"]?.ToString(), out _)
 			) {
-				switch (kind) {
-					case AssetType.person:
-						asset = new Person() {
-							General = obj.ToObject<PersonGeneral>(serializer),
-						};
-						break;
-					case AssetType.vehicle:
-						asset = new Vehicle() {
-							General = obj.ToObject<VehicleGeneral>(serializer),
-						};
-						break;
-					case AssetType.trailer:
-						asset = new Trailer() {
-							General = obj.ToObject<TrailerGeneral>(serializer),
-						};
-						break;
-					case AssetType.asset:
-						asset = new Asset() {
-							General = obj.ToObject<AssetGeneral>(serializer),
-						};
-						break;
-				}
-				asset.General.deleted = true;
+				asset = new Asset() {
+					General = obj.ToObject<AssetGeneral>(serializer),
+				};
 			} else {
-				switch (kind) {
-					case AssetType.person:
-						asset = new Person() {
-							General = obj.ToObject<PersonGeneral>(serializer),
-							Advanced = obj.ToObject<AssetAdvanced>(serializer),
-							Dispatch = obj["dispatch"].ToObject<AssetDispatch>(serializer),
-						};
-						break;
-					case AssetType.vehicle:
-						asset = new Vehicle() {
-							General = obj.ToObject<VehicleGeneral>(serializer),
-							Advanced = obj.ToObject<VehicleAdvanced>(serializer),
-							Dispatch = obj["dispatch"].ToObject<AssetDispatch>(serializer),
-						};
-						break;
-					case AssetType.trailer:
-						asset = new Trailer() {
-							General = obj.ToObject<TrailerGeneral>(serializer),
-							Advanced = obj.ToObject<AssetAdvanced>(serializer),
-							Dispatch = obj["dispatch"].ToObject<AssetDispatch>(serializer),
-						};
-						break;
-					case AssetType.asset:
-						asset = new Asset() {
-							General = obj.ToObject<AssetGeneral>(serializer),
-							Advanced = obj.ToObject<AssetAdvanced>(serializer),
-							Dispatch = obj["dispatch"].ToObject<AssetDispatch>(serializer),
-						};
-						break;
-				}
-				asset.Dispatch.id = asset.id;
-				asset.Dispatch.company = asset.company;
+				asset = new Asset() {
+					General = obj.ToObject<AssetGeneral>(serializer),
+					Advanced = obj.ToObject<AssetAdvanced>(serializer),
+					dispatch = obj["dispatch"].ToObject<AssetDispatch>(serializer),
+				};
+				asset.dispatch.id = asset.id;
+				asset.dispatch.company = asset.company;
 				asset.v = obj["v"].Select(p => (int)p).ToArray();
 			}
-
 			return asset;
 		}
 	}
